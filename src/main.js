1f218f286debbb31fa5fc1032cd5d5791c0be61a
tsy <tsy821@qq.com>
2023-05-28 13:25:47 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import tsyvue from 'tsyvue'
 
-createApp(App).use(router).mount('#app')
+
+
+createApp(App).use(router).use(tsyvue).mount('#app')
